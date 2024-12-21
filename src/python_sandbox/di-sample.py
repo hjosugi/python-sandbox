517a76a9c0c7c19c8d8475a0ff8572d8a8c14d01
hjosugi <h.jo.sugi@gmail.com>
2024-12-21 13:35:43 +0900

    adapt di pattern

--- src/python_sandbox/di-sample.py
+++ src/python_sandbox/di-sample.py
@@ -1,18 +1,37 @@
-class DBUserRepository:
+from abc import ABCMeta, abstractmethod
+
+
+class User:
+    def __init__(self, id: int, name: str, email: str):
+        self.id = id
+        self.name = name
+        self.email = email
+
+    def __getitem__(self, key):
+        return getattr(self, key)
+
+
+class UserRepository(metaclass=ABCMeta):
+    @abstractmethod
     def get_user_by_id(self, user_id):
-        user = {"id": user_id, "name": "John Doe", "email": ""}
+        pass
+
+
+class DBUserRepository(UserRepository):
+    def get_user_by_id(self, user_id):
+        user = User(user_id, "John Doe", "")
         return user
 
 
-class APIUserRepository:
-    def fetch_user_by_id(self, user_id):
-        user = {"id": user_id, "name": "John Doe", "email": ""}
+class APIUserRepository(UserRepository):
+    def get_user_by_id(self, user_id) -> User:
+        user = User(user_id, "John Doe", "")
         return user
 
 
 class Service:
-    def __init__(self):
-        self.user_repository = DBUserRepository()
+    def __init__(self, user_repository):
+        self.user_repository = user_repository
 
     def display_user(self, user_id):
         user = self.user_repository.get_user_by_id(user_id)
@@ -20,7 +39,8 @@ class Service:
 
 
 def main():
-    service = Service()
+    user_repository = APIUserRepository()
+    service = Service(user_repository)
     service.display_user(1)
 
 
